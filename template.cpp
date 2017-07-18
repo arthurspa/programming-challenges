4eb840290960a8aec8c782525eb4425a7afc8a6a
Arthur Silva <arthursilva.ee@gmail.com>
2017-07-17 23:25:25 -0400

    Added template file

---
+++ template.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <stdio.h>	// stdin
+#include <cstdlib> // getenv
+
+using namespace std;
+
+
+
+void solve(){
+	// My solution goes here
+
+}
+
+int main()
+{
+	if(getenv("IS_DEBUG"))
+		FILE* f = freopen("input", "r", stdin);
+
+	solve();    
+
+	// Workaround for issue 
+	// https://github.com/Microsoft/vscode-cpptools/issues/466
+	if(getenv("IS_DEBUG"))
+		cout << endl;
+
+	return 0;
+}
