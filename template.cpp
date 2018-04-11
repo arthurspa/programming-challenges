3262bff9110d6739367522adcae5cc7cf09a2566
Arthur Silva <arthursilva.ee@gmail.com>
2018-04-10 23:55:28 -0400

    Format template

--- template.cpp
+++ template.cpp
@@ -1,27 +1,36 @@
 #include <iostream>
-#include <stdio.h>	// stdin
+#include <stdio.h> // stdin
 #include <cstdlib> // getenv
+#include <algorithm>
+#include <vector>
+#include <map>
+#include <string>
 
 using namespace std;
 
-
-
-void solve(){
-	// My solution goes here
-
+void solve()
+{
+    // My solution goes here
 }
 
 int main()
 {
-	if(getenv("IS_DEBUG"))
-		FILE* f = freopen("input", "r", stdin);
 
-	solve();    
+    if (getenv("IS_DEBUG"))
+    {
+        string fullFileName(__FILE__);
+        string fileNameWithoutExtension = fullFileName.substr(0, fullFileName.rfind('.'));
+        string inputFileName = fileNameWithoutExtension.append(".input");
 
-	// Workaround for issue 
-	// https://github.com/Microsoft/vscode-cpptools/issues/466
-	if(getenv("IS_DEBUG"))
-		cout << endl;
+        freopen(inputFileName.c_str(), "r", stdin);
+    }
 
-	return 0;
+    solve();
+
+    // Workaround for issue
+    // https://github.com/Microsoft/vscode-cpptools/issues/466
+    if (getenv("IS_DEBUG"))
+        cout << endl;
+
+    return 0;
 }
