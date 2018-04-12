05a04e02d5c9031e174ffd93f534672bff9d2f6d
Arthur Silva <arthursilva.ee@gmail.com>
2018-04-12 01:02:21 -0400

    Improve template

--- template.cpp
+++ template.cpp
@@ -4,9 +4,26 @@
 #include <algorithm>
 #include <vector>
 #include <map>
+#include <cmath>
 #include <string>
 
 using namespace std;
+
+#define FOR(i,a,b) for(int i = (a); i <= (b); ++i)
+#define FORR(i,a,b) for(int i = (a); i >= (b); --i)
+#define REP(i,n) FOR(i,0,(n)-1)
+#define REP1(i,n) FOR(i,1,(n))
+#define mp make_pair
+#define pb push_back
+#define st first
+#define nd second
+#define sz(w) (int) w.size()
+typedef vector<int> vi;
+typedef long long ll;
+typedef long double ld;
+typedef pair<int,int> pii;
+const int inf = 1e9 + 5;
+
 
 void solve()
 {
