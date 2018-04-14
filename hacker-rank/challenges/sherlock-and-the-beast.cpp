1b1d1108d9474813b61c7a158c8eeac2fede17e1
Arthur Silva <arthursilva.ee@gmail.com>
2018-04-14 02:51:39 -0400

    [HackerRank] sherlock-and-the-beast

---
+++ hacker-rank/challenges/sherlock-and-the-beast.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <stdio.h> // stdin
+#include <cstdlib> // getenv
+#include <algorithm>
+#include <vector>
+#include <map>
+#include <cmath>
+#include <string>
+#include <ctime>
+
+using namespace std;
+
+#define FOR(i, a, b) for (int i = (a); i <= (b); ++i)
+#define FORR(i, a, b) for (int i = (a); i >= (b); --i)
+#define REP(i, n) FOR(i, 0, (n)-1)
+#define REP1(i, n) FOR(i, 1, (n))
+#define mp make_pair
+#define pb push_back
+#define st first
+#define nd second
+#define sz(w) (int)w.size()
+typedef vector<int> vi;
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+const int inf = 1e9 + 5;
+
+void solve()
+{
+    int N, k, fives, threes;
+    bool found;
+
+    cin >> N;
+
+    REP(i, N)
+    {
+        cin >> k;
+
+        fives = k;
+        threes = 0;
+        while( fives % 3 > 0 || threes % 5 > 0 && k >= 0){
+            fives--;
+            threes++;
+            k--;
+        }
+
+        if (k >= 0)
+        {
+            REP(i, fives)
+                cout << "5";
+            REP(i, threes)
+                cout << "3";
+            cout << endl;
+        }
+        else
+        {
+            cout << "-1" << endl;
+        }
+    }
+}
+
+int main()
+{
+
+    if (getenv("IS_DEBUG"))
+    {
+        string fullFileName(__FILE__);
+        string fileNameWithoutExtension = fullFileName.substr(0, fullFileName.rfind('.'));
+        string inputFileName = fileNameWithoutExtension.append(".input");
+
+        freopen(inputFileName.c_str(), "r", stdin);
+    }
+
+    int start = clock();
+    solve();
+    int end = clock();
+
+    // Workaround for issue
+    // https://github.com/Microsoft/vscode-cpptools/issues/466
+    if (getenv("IS_DEBUG"))
+    {
+        cout << endl
+             << "[Finished in " << end - start << " ms]" << endl;
+    }
+
+    return 0;
+}
