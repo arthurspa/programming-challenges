b86231135879b48248fe63485f6f9e6a954bd678
Arthur Silva <arthursilva.ee@gmail.com>
2018-04-19 22:50:39 -0400

    Format template

--- template.cpp
+++ template.cpp
@@ -7,24 +7,28 @@
 #include <cmath>
 #include <string>
 #include <ctime>
+#include <deque>
+#include <queue>
+#include <set>
+#include <list>
+#include <stack>
 
 using namespace std;
 
-#define FOR(i,a,b) for(int i = (a); i <= (b); ++i)
-#define FORR(i,a,b) for(int i = (a); i >= (b); --i)
-#define REP(i,n) FOR(i,0,(n)-1)
-#define REP1(i,n) FOR(i,1,(n))
+#define FOR(i, a, b) for (int i = (a); i <= (b); ++i)
+#define FORR(i, a, b) for (int i = (a); i >= (b); --i)
+#define REP(i, n) FOR(i, 0, (n)-1)
+#define REP1(i, n) FOR(i, 1, (n))
 #define mp make_pair
 #define pb push_back
 #define st first
 #define nd second
-#define sz(w) (int) w.size()
+#define sz(w) (int)w.size()
 typedef vector<int> vi;
 typedef long long ll;
 typedef long double ld;
-typedef pair<int,int> pii;
+typedef pair<int, int> pii;
 const int inf = 1e9 + 5;
-
 
 void solve()
 {
@@ -47,13 +51,14 @@ int main()
     int start = clock();
     solve();
     int end = clock();
-    
+
     // Workaround for issue
     // https://github.com/Microsoft/vscode-cpptools/issues/466
-    if (getenv("IS_DEBUG")){
-        cout << endl << "[Finished in " << end - start << " ms]" << endl;
+    if (getenv("IS_DEBUG"))
+    {
+        cout << endl
+             << "[Finished in " << end - start << " ms]" << endl;
     }
-        
 
     return 0;
 }
