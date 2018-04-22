31a50e6957e364a37097e4520c0340717be151c8
Arthur Silva <arthursilva.ee@gmail.com>
2018-04-22 16:59:14 -0400

    [HackerRank] detect-whether-a-linked-list-contains-a-cycle

---
+++ hacker-rank/challenges/detect-whether-a-linked-list-contains-a-cycle.cpp
@@ -0,0 +1,30 @@
+/*
+Detect a cycle in a linked list. Note that the head pointer may be 'NULL' if the list is empty.
+
+A Node is defined as: 
+    struct Node {
+        int data;
+        struct Node* next;
+    }
+*/
+
+bool has_cycle(Node* head) {
+    // Complete this function
+    // Do not write the main method
+    
+    if(head == nullptr){
+        return false;
+    }
+    
+    Node *slow = head;
+    Node *fast = head;
+    while(fast != nullptr && fast->next != nullptr){
+        slow = slow->next;
+        fast = fast->next->next;
+        
+        if(slow == fast){
+            return true;
+        }
+    }
+    return false;
+}
