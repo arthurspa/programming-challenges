f36da720443cffb784c14c3b15009914dc016233
Arthur Silva <arthursilva.ee@gmail.com>
2018-05-15 01:35:09 -0400

    Improve template

--- template.cpp
+++ template.cpp
@@ -2,11 +2,14 @@
 #include <stdio.h> // stdin
 #include <cstdlib> // getenv
 #include <algorithm>
+#include <string>
+#include <sstream>
+#include <cmath>
+#include <cstring>
+#include <cassert>
+#include <ctime>
 #include <vector>
 #include <map>
-#include <cmath>
-#include <string>
-#include <ctime>
 #include <deque>
 #include <queue>
 #include <set>
@@ -15,25 +18,29 @@
 
 using namespace std;
 
-#define FOR(i, a, b) for (int i = (a); i <= (b); ++i)
-#define FORR(i, a, b) for (int i = (a); i >= (b); --i)
-#define REP(i, n) FOR(i, 0, (n)-1)
-#define REP1(i, n) FOR(i, 1, (n))
-#define mp make_pair
 #define pb push_back
-#define st first
-#define nd second
-#define sz(w) (int)w.size()
-typedef vector<int> vi;
-typedef long long ll;
-typedef long double ld;
-typedef pair<int, int> pii;
-const int inf = 1e9 + 5;
+#define mp make_pair
+#define pii pair<int, int>
+#define vi vector<int>
+#define vpii vector<pii>
+#define SZ(x) ((int)(x.size()))
+#define fi first
+#define se second
+#define FOR(i, n) for (int(i) = 0; (i) < (n); ++(i))
+#define FORI(i, n) for (int(i) = 1; (i) <= (n); ++(i))
+#define IN(x, y) ((y).find((x)) != (y).end())
+#define ALL(t) t.begin(), t.end()
+#define FOREACH(i, t) for (typeof(t.begin()) i = t.begin(); i != t.end(); i++)
+#define REP(i, a, b) for (int(i) = (a); (i) <= (b); ++i)
+#define REPD(i, a, b) for (int(i) = (a); (i) >= (b); --i)
+#define REMAX(a, b) (a) = max((a), (b));
+#define REMIN(a, b) (a) = min((a), (b));
+#define DBG cerr << "debug here" << endl;
+#define DBGV(vari) cerr << #vari << " = " << (vari) << endl;
 
 void solve()
 {
     // My solution goes here
-    
 }
 
 int main()
